261d4c3fc1a90b3337de934980f9c0b2fd68f17e
Yang Yang <yy90axf2010@gmail.com>
2016-03-07 14:11:27 -0400

    view_detail updated

--- app/src/main/java/com/example/colini/mobilecomputingproject/View_Detial.java
+++ app/src/main/java/com/example/colini/mobilecomputingproject/View_Detial.java
@@ -6,16 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Yang on 16-03-06.
  */
 public class View_Detial extends Fragment {
 
+    private Button button;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.view_detail,container,false);
+        button = (Button) rootView.findViewById(R.id.DetailButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.mainContainer,new mainFragment()).commit();
+            }
+        });
+
         return rootView;
     }
 }
